8b727e73e91d7fa692d24d27083171e412272f7c
Marlene Emilia <marlene@sylph.yuuki.ml>
2018-11-07 15:01:51 -0600

    initial scripting I've decided that this project is going to be on Python! Initial files have been created and scripting is going well.

---
+++ file.py
@@ -0,0 +1,8 @@
+from beautifulscraper import BeautifulScraper
+
+web = BeautifulScraper()
+
+page = web.go("https://playoverwatch.com/en-us/career/pc/himokko413-1907")
+file = open("pc-prettified.html","w")
+file.write(page.prettify().encode('utf-8'));
+file.close
---
+++ main.py
@@ -0,0 +1,2 @@
+# This program requires BeautifulScraper (installable on pip).
+# If it doesn't run, check that you've installed it.
